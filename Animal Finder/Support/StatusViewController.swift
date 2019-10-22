e76b7d92b74575f1fdfa16f58d1a664a437162a5
Jake Lancaster <jake.5330@hotmail.com>
2019-10-22 16:26:49 +0100

    added new files and began making status view controller class

---
+++ Animal Finder/Support/StatusViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StatusViewController.swift
+//  Animal Finder
+//
+//  Created by Jake on 22/10/19.
+//  Copyright © 2019 Jake. All rights reserved.
+//
+
+import Foundation
+import ARKit
+
+class StatusViewController: UIViewController {
+    
+}
---
+++ Animal Finder/Support/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  Animal Finder
+//
+//  Created by Jake on 22/10/19.
+//  Copyright © 2019 Jake. All rights reserved.
+//
+
+import Foundation
